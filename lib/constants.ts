a959ec439d84d1e636a8dfc12931609b42cc9903
Moumennedjar <mmnnedjar@gmail>
2026-02-24 12:59:03 +0100

    initial commit

---
+++ lib/constants.ts
@@ -0,0 +1,50 @@
+export const events = [
+  {
+    image: '/images/event1.png',
+    title: 'React Summit 2026',
+    slug: 'react-summit-2026',
+    location: 'San Francisco, CA',
+    date: '2026-05-10',
+    time: '10:00 AM'
+  },
+  {
+    image: '/images/event2.png',
+    title: 'Next.js Conference',
+    slug: 'nextjs-conference-2026',
+    location: 'New York, NY',
+    date: '2026-06-15',
+    time: '09:30 AM'
+  },
+  {
+    image: '/images/event3.png',
+    title: 'JS World Meetup',
+    slug: 'js-world-meetup-2026',
+    location: 'London, UK',
+    date: '2026-07-20',
+    time: '11:00 AM'
+  },
+  {
+    image: '/images/event4.png',
+    title: 'Frontend Masters Workshop',
+    slug: 'frontend-masters-workshop-2026',
+    location: 'Berlin, Germany',
+    date: '2026-08-05',
+    time: '02:00 PM'
+  },
+  {
+    image: '/images/event5.png',
+    title: 'Fullstack Dev Conference',
+    slug: 'fullstack-dev-conference-2026',
+    location: 'Toronto, Canada',
+    date: '2026-09-12',
+    time: '09:00 AM'
+  },
+  {
+    image: '/images/event6.png',
+    title: 'Hackathon Europe',
+    slug: 'hackathon-europe-2026',
+    location: 'Paris, France',
+    date: '2026-10-25',
+    time: '12:00 PM'
+  }
+];
